a3df3ac33221a1b511d84adbbfebb4ea4a160f1e
Isaura Almar <isauraalmar@gmail.com>
2016-10-12 14:16:57 +0200

    Category and products sumary

--- System-Integration_Compulsory/Models/Category.cs
+++ System-Integration_Compulsory/Models/Category.cs
@@ -1,16 +1,16 @@
 ﻿namespace System_Integration_Compulsory.Models
 {
     /// <summary>
-    /// 
+    /// Category of products
     /// </summary>
     public class Category
     {
         /// <summary>
-        /// 
+        /// The unique identifier
         /// </summary>
         public long Id { get; set; }
         /// <summary>
-        /// 
+        /// The category name
         /// </summary>
         public string Name { get; set; }
     }
--- System-Integration_Compulsory/Models/Product.cs
+++ System-Integration_Compulsory/Models/Product.cs
@@ -4,29 +4,29 @@ using System.ComponentModel.DataAnnotations;
 namespace System_Integration_Compulsory.Models
 {
     /// <summary>
-    /// 
+    /// Product Attributes 
     /// </summary>
     public class Product
     {
         /// <summary>
-        /// 
+        /// The unique identifier
         /// </summary>
         [Required]
         public long Id { get; set; }
         /// <summary>
-        /// 
+        /// The name of the product
         /// </summary>
         public string Name { get; set; }
         /// <summary>
-        /// 
+        /// The Price for one item of the product
         /// </summary>
         public decimal Price { get; set; }
         /// <summary>
-        /// 
+        /// In which category the product belongs
         /// </summary>
         public Category Category { get; set; }
         /// <summary>
-        /// 
+        /// The how many items there are in stock
         /// </summary>
         public long NumberInStock { get; set; }
     }
